015b1785e9c90308b98e84d483fc52dfa67e9d62
zaid-khatib8 <103290793+zaid-khatib8@users.noreply.github.com>
2023-12-11 22:26:47 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,147 @@
+# Final Project: Belay (a Slack clone)
+
+40 points
+## NOTE: I collaborated with a peer in the course, Keval Hutheesing, and used the following resources to aid with various aspects of this project:
+- Full Stack Realtime Chat App Tutorial: https://www.youtube.com/watch?v=MJzbJQLGehs
+- jQuery tutorial: https://www.w3schools.com/jquery/
+- Building Slack UI with pure HTML5, CSS3 and JavaScript: https://dev.to/sirneij/building-slack-ui-with-pure-html5-css3-and-javascript-the-power-of-css-grids-and-flexbox-4ban
+- How to Use Fetch with Flask in JavaScript/Python: https://www.youtube.com/watch?v=exRAM1ZWm_s&t=9s
+
+**DUE: Friday, March 10 by 5:30pm**
+
+Create your own repo at: https://classroom.github.com/a/XbBPcBk8
+
+## Introduction
+
+As a capstone project for Web Development, we're going to combine the various front-end and back-end 
+techniques we've learned over the course to produce a modern, database-backed single-page 
+application. Specifically, we'll be building our own (significantly smaller in scope) version of the 
+popular workplace messaging app Slack. We'll call our version 
+[Belay](https://en.wikipedia.org/wiki/Belaying).
+
+## Core Behavior
+
+- Belay lets users send and read real-time chat messages that are organized into rooms called 
+  Channels. Users see a list of all the channels on the server and can click one to enter that 
+  channel. Inside, they see all the messages posted to that channel by any user, and can post their 
+  own messages. All messages belong to a channel and all channels are visible to all users; we don't 
+  need to implement private rooms or direct messages.
+- Any user can create a new channel by supplying a display name. Channel names must be unique. If 
+  you wish, You may choose to limit what characters are allowed in channel names.
+- Like Slack, messages may be threaded as Replies in response to a message in a channel. Messages in 
+  the channel will display how many replies they have if that number is greater than zero. We don't
+  support nested threads; messages either belong directly to a channel or are replies in a thread to 
+  a message that does, but replies can't have nested replies of their own.
+
+## Submissions and Grading
+
+Graders will have Python 3.11+ with Flask installed, and a local install of SQLite3 (which comes 
+with Python). Because graders must use the same environment to evaluate submissions from multiple 
+students, please do not require any additional programs or packages to be installed. In your 
+submission, include a README with instructions for how to configure and run your app:
+- Graders will start your app with a `flask run` command from the command line. Graders will have 
+  their FLASK_APP environment variable set to "app," so name your Flask file `app.py`.
+- Graders will have the packages in `requirements.txt` installed with `pip3 install
+  -r requirements.txt`. If you feel strongly that you need a package not listed there, ask on the 
+  course Slack.
+- Graders will try to access your app in their browser at the URL that Flask prints to the command 
+  line, e.g. `* Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)`
+- Make sure that your app starts successfully under these circumstances. We'll do our best to make 
+  your app run even if it doesn't, but with a points penalty.
+
+You can use any techniques or tools to complete your project, whether or not we studied them in 
+class. In particular, note that there is no requirement that you use React (though it may make the 
+project easier to complete). You could do the whole project with vanilla Javascript, you could use
+React, or you could use some other framework we didn't study in class, like 
+[Vue.js](https://vuejs.org/) or [Svelte](https://svelte.dev/). As always, remember to include in 
+your submission any classmates you collaborated with and any materials you consulted.
+
+## Requirements and Rubric (40 points total)
+
+### Unauthenticated UI: (3 points)
+- Unanthenticated users can create a new account
+- Unauthenticated users can sign in with their username and password
+- Unauthenticed users who try to access a room cannot see any messages in that room, and are sent to 
+  the signup/login page instead
+
+### Authenticated UI: (10 points)
+- Authenticated users can log out, change their username, and change their password
+- Can see a list of all channels. For each channel, can see how many unread messages the user has
+- Visiting a channel marks all messages in it as read, and all new messages posted to that channel 
+  while the user is in it are marked as read too
+- Check for new messages in the channel at least once every 500 ms. Stop checking if the user leaves 
+  the channel. (Hint: use SetInterval)
+- Check for new unread messages in other channels at least once every second. Use only one HTTP 
+  request to get counts for all channels 
+- For each message with replies, display the number of replies to that message just below the 
+  message content
+- All messages in a room have a Reply button or icon that opens the replies pane
+- Parse image URLs that appear in messages and display the images at the end of
+  the message. Hint: you may use the web to help you find an appropriate regular
+  expression.
+- Users can add an emoji reaction to any message or reply
+- Hovering over a reaction displays all the users who had that reaction
+
+### Single-Page State (5 points)
+- Only serve one HTML request—handle all other requests through the API
+- Push the channel name (for messages) or parent message id (for replies) to the history and 
+  navigation bar when the user navigates to a channel or thread. Users can use the Back button to 
+  navigate to a previous channel or thread
+- Loading the unique URL of a channel or thread should open the app to that
+  channel or thread.
+- If an unauthenticated user follows a link to a channel or thread, show them the login or signup 
+  screens, but if they log in or sign up, send them to the original page they requested.
+- Save the user's auth key in localStorage or in a cookie. Include your CNETID
+  as part of your storage keys so your storage won't conflict with
+  those of other students on the graders' machines. e.g.
+  `window.localStorage.setItem('trevoraustin_belay_auth_key', 'abcdefg')`
+
+### Responsive Styling: (8 points)
+Wide Screen:
+- Show the list of channels down the left-hand side of the screen, and the channel 
+  the user is looking at (or a placeholder for no channel) on the right-hand side
+- Clicking on the name of a channel loads that channel's messages into the right-hand column
+- The current channel is highlighted in the channel list
+- The names of other channels change have a subtle visual change on hover
+- When viewing a reply thread, display the thread as a third column, narrowing the 
+  column with messages
+- Users can click an icon or button to dismiss the thread panel
+- On narrow screens, the page has a one-column layout with a menu bar. Users see the channel list,
+  the messages in one channel, or the replies to one message at a time, and not the other two
+- When viewing replies, users can see the parent message they are replying to.
+  They can click a button or link to navigate to the channel containing the
+  parent message
+- When viewing messages in a channel on a narrow screen, users have a button or
+  link they can click to navigate back to the channel list.
+
+
+### Database (8 points)
+- Store channels, messages, and user account information in a SQLite3 database
+- Create the database and its tables with migrations Start the name(s) of your migration file(s) with 
+  [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) timestamps so that the file system will 
+  list them in the order they were created. Check those migrations into version control for your 
+  assignment, alongside the actual database file
+- Create a table for channels that stores a unique id and the channel name.
+- Create a table for messages that stores, at a minimum, what channel the message is in, the id of 
+  the user that wrote the message, and its text contents
+- Store Replies in the Messages table. Implement a way of distinguishing regular messages in a 
+  channel from replies to a message (e.g. with a `replies_to` column that is null for normal 
+  messages but contains a messsage_id for Replies)
+- Create a table for reactions that stores, at a minimum, the emoji, the id of the message or 
+  comment it is a reaction to, and the id of the user who made the reaction
+- Create a [join table](https://stackoverflow.com/questions/16549971/join-tables-in-sqlite-with-many-to-many)
+  to capture the many-to-many relationship between Users and which Messages they have seen. (Hint: 
+  store the *latest* timestamp or message id seen for each user in each channel—you don't have to 
+- Sanitize all database inputs using prepared statements
+
+### API (6 points)
+- Give API endpoints a unique path namespace to distinguish them from your HTML
+  path(s) e.g. `/api/endpoint1`, `/api/encpoint2` etc.
+- Authentication endpoint that accepts a username and password, and returns a session token
+- Authenticate to other endpoints via session token in the request header (not
+  as a URL param or in a request body)
+- Use GET requests for API calls that don't change data on the server, and POST requests for API 
+  calls that **do** change data on the server
+- Endpoints to create and get channels and messages, and to update a user's last read message in a 
+  channel
+- Endpoint to return unread message counts for the user for each channel in a single request
---
+++ app (2).py
@@ -0,0 +1,362 @@
+from flask import Flask, render_template, request, jsonify
+from functools import wraps
+import configparser
+import io
+import datetime
+from datetime import timedelta
+import json
+import string
+import random
+import sqlite3
+from flask import * # Flask, g, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+
+# Database management functions:
+def dict_from_row(row):
+    return dict(zip(row.keys(), row)) 
+        
+
+def dict_from_list_from_row(list):
+    messages = {}
+    for i in range (len(list)):
+        messages.update({"message" + str(i):dict_from_row(list[i])})
+    return messages
+
+def get_db():
+    db = getattr(g, '_database', None)
+
+    if db is None:
+        db = g._database = sqlite3.connect('db/belay.sqlite3')
+        db.row_factory = sqlite3.Row
+        setattr(g, '_database', db)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    db = get_db()
+    cursor = db.execute(query, args)
+    # print("query_db")
+    # print(cursor)
+    rows = cursor.fetchall()
+    # print(rows)
+    db.commit()
+    cursor.close()
+    if rows:
+        if one: 
+            return rows[0]
+        return rows
+    return None
+
+
+#Helper functions
+session_tokens = set()
+def generate_session_token():
+    session_token =  ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+    while (session_token in session_tokens):
+        session_token =  ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+    session_tokens.add(session_token)
+    return session_token
+
+@app.route('/')
+def index(chat_id=None):
+    return app.send_static_file('index.html')
+
+
+"""
+-------------------------
+-------------------------
+-------------------------
+"""
+# Login and signup API's
+@app.route('/api/login', methods = ['POST'])
+def login():
+    username = request.get_json()['username']
+    password = request.get_json()['password']
+    row = query_db('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], one = True)
+    row_dict = row
+    if row is not None:
+        row_dict = dict_from_row(row)
+    else:
+        return {}, 300
+    token = generate_session_token()
+    return {"data": row_dict, "token": token}, 200
+
+@app.route('/api/signup', methods=['POST'])
+def signup ():
+    username = request.get_json()['username']
+    password = request.get_json()['password']
+    api_key = ''.join(random.choices(string.ascii_lowercase + string.digits, k=40))
+    query_db("INSERT INTO users(username, password, api_key) VALUES(?, ?, ?)", [username, password, api_key])
+    return {}, 200
+
+
+
+"""
+-------------------------
+-------------------------
+-------------------------
+"""
+# Change username and change password API's
+@app.route('/api/changeusername', methods=['POST'])
+def changeusername():
+    user_id = request.get_json()['user_id']
+    new_username = request.get_json()['new_username']
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):
+        query_db("UPDATE users SET username = ? WHERE user_id = ?", [new_username, user_id])
+        return {}, 200
+    return {}, 300
+
+@app.route('/api/changepassword', methods=['POST'])
+def changepassword():
+    user_id = request.get_json()['user_id']
+    new_password = request.get_json()['new_password']
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):
+        query_db("UPDATE users SET password = ? WHERE user_id = ?", [new_password, user_id])
+        return {}, 200
+    return {}, 300
+
+
+
+
+@app.route('/api/displaychannelnames', methods=['GET'])
+def displaychannelnames ():
+    user_id = request.headers.get('user_id')
+    print(user_id)
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):
+        channels = query_db("SELECT * FROM channels")
+        channels_dict = channels
+        if channels is not None:
+            channels_dict = dict_from_list_from_row(channels)
+        return jsonify(channels_dict), 200
+    else:
+        return {}, 300
+
+@app.route('/api/loadmessages', methods=['GET'])
+def loadmessages():
+    print("Are we even making it to loadmessages????")
+    user_id = request.headers.get('user_id')
+    channel_name = request.headers.get('channel_name')
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):
+        channel_id_query = query_db('SELECT channel_id FROM channels WHERE channel_name = ?', [channel_name], one=True)
+        channel_id_dict = dict_from_row(channel_id_query)
+        ch_id = channel_id_dict['channel_id']
+        msg_query = query_db("SELECT u.username, m.message_id, m.time_stamp, m.body FROM messages m LEFT JOIN users u ON m.user_id = u.user_id WHERE m.channel_id = ? AND m.reply_id IS NULL ORDER BY m.message_id DESC", [ch_id])
+        msg_dict = msg_query
+        if msg_query is None:
+            messages = {}
+            return jsonify(messages), 200
+        msg_dict = dict_from_list_from_row(msg_query)
+        messages = {"content": dict_from_list_from_row(msg_query)}
+
+        count_query = query_db("SELECT reply_id, COUNT(*) AS cnt FROM messages WHERE channel_id = ? GROUP BY reply_id", [ch_id])
+        count_dict = count_query
+        count_dict = dict_from_list_from_row(count_query)   
+        messages["count"] = count_dict
+        return jsonify(messages), 200
+    else:
+        return {}, 300
+
+
+@app.route('/api/getmessages', methods=['GET'])
+def getmessages():
+    #print("----- calling getmessage method!")
+    user_id = request.headers.get('user_id')
+    channel_name = request.headers.get('channel_name')
+    #print('for channel')
+    #print(channel_name)
+    last_id = request.headers.get('last_msg_ID')
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):        
+        channel_id_query = query_db('SELECT channel_id FROM channels WHERE channel_name = ?', [channel_name], one=True)
+        channel_id_dict = dict_from_row(channel_id_query)
+        #print(channel_id_dict)
+        channel_id = channel_id_dict['channel_id']
+        msg_query = query_db("SELECT u.username, m.message_id, m.time_stamp, m.body FROM messages m LEFT JOIN users u ON m.user_id = u.user_id WHERE m.channel_id = ? AND m.reply_id IS NULL AND m.message_id > ? ORDER BY m.message_id DESC", [channel_id, last_id])
+        #print(msg_query)
+        message_dict = msg_query
+        #print(message_dict)
+        if msg_query is None:
+            messages = {}
+            return jsonify(messages), 200
+        #print('Are we making it to here?')
+        message_dict = dict_from_list_from_row(msg_query)
+        messages = {"content": message_dict}
+
+        count_query = query_db("SELECT reply_id, COUNT(*) AS cnt FROM messages WHERE channel_id = ? GROUP BY reply_id", [channel_id])
+        count = dict_from_list_from_row(count_query)
+        messages["count"] = count
+        return jsonify(messages), 200
+    else:
+        return {}, 300
+
+
+@app.route('/api/loadreplies', methods=['GET'])
+def loadreplies():
+    user_id = request.headers.get('user_id')
+    message_id = request.headers.get('message_id')
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):
+        msg_query = query_db("SELECT u.username, m.message_id, m.time_stamp, m.body FROM messages m LEFT JOIN users u ON m.user_id = u.user_id WHERE m.reply_id = ? ORDER BY m.message_id", [message_id])
+        message_dict = msg_query
+        if msg_query is not None:
+            message_dict = dict_from_list_from_row(msg_query)
+        messages = message_dict
+        #print(messages)
+        return jsonify(messages), 200
+    else:
+        return {}, 300
+
+
+@app.route('/api/createchannel', methods=['POST'])
+def createchannel ():
+    user_id = request.headers.get('user_id')
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):
+        channel_name = request.get_json()['channel_name']
+        query_db("INSERT INTO channels(channel_name) VALUES (?)", [channel_name])
+        return {}, 200
+    else:
+        return {}, 300
+
+@app.route('/api/postmessage', methods=['POST'])
+def postmessage():
+    user_id = request.headers.get('user_id')
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):
+        channel_name = request.get_json()['channel_name']
+        message = request.get_json()['message']
+        channel_id_query = query_db('SELECT channel_id FROM channels WHERE channel_name = ?', [channel_name], one=True)
+        channel_id_dict = dict_from_row(channel_id_query)
+        channel_id = channel_id_dict['channel_id']
+        query_db("INSERT INTO messages(user_id, channel_id, body) VALUES(?, ?, ?)", [user_id, channel_id, message])
+        return {}, 200
+    else:
+        return {}, 300
+
+
+@app.route('/api/postreply', methods=['POST'])
+def postreply():
+    user_id = request.headers.get('user_id')
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):
+        channel_name = request.get_json()['channel_name']
+        channel_id_query = query_db('SELECT channel_id FROM channels WHERE channel_name = ?', [channel_name], one=True)
+        channel_id_dict = dict_from_row(channel_id_query)
+        channel_id = channel_id_dict['channel_id']
+        message = request.get_json()['message']
+        reply_id = request.get_json()['reply_id']
+        query_db("INSERT INTO messages(user_id, channel_id, body, reply_id) VALUES(?, ?, ?, ?)", [user_id, channel_id, message, reply_id])
+        return {}, 200
+    else:
+        return {}, 300
+
+
+# Add emoji APIs
+@app.route('/api/addemoji', methods=['POST'])
+def addemoji():
+    user_id = request.headers.get('user_id')
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):
+        emoji = request.get_json()['emoji'] 
+        message_id = request.get_json()['message_id']
+        row = query_db('SELECT * FROM reactions WHERE emoji = ? AND message_id = ? AND user_id = ?', [emoji, message_id, user_id])
+        if row is None:
+            query_db("INSERT INTO reactions(emoji, message_id, user_id) VALUES(?, ?, ?)", [emoji, message_id, user_id])
+        return {}, 200
+    else:
+        return {}, 300
+
+@app.route('/api/showreactions', methods=['POST'])
+def showreactions():
+    print('calling show reactions')
+    user_id = request.headers.get('user_id')
+    print('user id is ' +  user_id)
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):
+        emoji = request.get_json()['emoji']
+        message_id = request.get_json()['message_id']
+        #user_id = request.get_json()['user_id']
+        usernames = query_db('SELECT username FROM (SELECT emoji, message_id, users.user_id, username FROM reactions INNER JOIN users ON reactions.user_id = users.user_id) WHERE message_id = ? AND emoji = ?', [message_id, emoji])
+        if usernames is None:
+            return {}, 300
+        user_dict = dict_from_list_from_row(usernames)
+        #print(user_dict)
+        return jsonify(user_dict), 200
+    else:
+        return {}, 301
+
+
+@app.route('/api/getunreadcount', methods=['GET'])
+def getunreadcount():
+    user_id = request.headers.get('user_id')
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):
+        channel_name = request.headers.get('channel_name')
+        channel_id_query = query_db('SELECT channel_id FROM channels WHERE channel_name = ?', [channel_name], one=True)
+        channel_id_dict = dict_from_row(channel_id_query)
+        channel_id = channel_id_dict['channel_id']
+        #print(channel_id)
+        last_id_query = query_db('SELECT last_viewed_id FROM last_viewed WHERE user_id = ? AND channel_id = ?', [user_id, channel_id], one=True)
+        last_id_dict = dict_from_row(last_id_query)
+        last_id = last_id_dict['last_viewed_id']
+        count_query = query_db("SELECT COUNT(*) AS count FROM messages m LEFT JOIN users u ON m.user_id = u.user_id WHERE m.channel_id = ? AND m.reply_id IS NULL AND m.message_id > ?", [channel_id, last_id], one=True)
+        count = count_query['count']
+        return jsonify(count), 200
+    else:
+        return {}, 300
+
+@app.route('/api/updatelastviewed', methods=['POST'])
+def updatelastviewed():
+    #print('calling updatelastviewed')
+    user_id = request.headers.get('user_id')
+    api_key = dict_from_list_from_row(query_db('SELECT api_key FROM users WHERE user_id = ?', [user_id]))   
+    if("Bearer "+ api_key['message0']['api_key']== request.headers.get('Authorization')):    
+        channel_name = request.get_json()['channel_name']
+        channel_id_query = query_db('SELECT channel_id FROM channels WHERE channel_name = ?', [channel_name], one=True)
+        channel_id_dict = dict_from_row(channel_id_query)
+        channel_id = channel_id_dict['channel_id']
+        last_id = request.get_json()['last_id']
+        #user_id = request.get_json()['user_id']
+        query_db('UPDATE last_viewed SET last_viewed_id = ? WHERE user_id = ? AND channel_id = ?', [last_id, user_id, channel_id])
+        return {}, 200
+    else:
+        return {},300
+
+@app.route('/api/initializelastviewedchannel', methods = ['POST'])
+def initializelastviewedchannel():
+    #print('calling initializelastviewed')
+    channel_name = request.get_json()['channel_name']
+    channel_id_query = query_db('SELECT channel_id FROM channels WHERE channel_name = ?', [channel_name], one=True)
+    channel_id_dict = dict_from_row(channel_id_query)
+    channel_id = channel_id_dict['channel_id']
+    user_ids = query_db('SELECT user_id FROM users')
+    for user_id_row in user_ids:
+        user_id_dict = dict_from_row(user_id_row)
+        query_db("INSERT INTO last_viewed (user_id, channel_id) VALUES (?, ?)", [user_id_dict['user_id'], channel_id])
+    return {}, 200
+
+@app.route('/api/initializelastvieweduser', methods = ['POST'])
+def initializelastvieweduser():
+    #print('calling initializelastvieweduser')
+    user_name = request.get_json()['user_name']
+    user_id_query = query_db('SELECT user_id FROM users WHERE username = ?', [user_name], one=True)
+    user_id_dict = dict_from_row(user_id_query)
+    user_id = user_id_dict['user_id']
+    channel_ids = query_db('SELECT channel_id FROM channels')
+    for channel_id_row in channel_ids:
+        channel_id_dict = dict_from_row(channel_id_row)
+        query_db("INSERT INTO last_viewed (user_id, channel_id) VALUES (?, ?)", [user_id, channel_id_dict['channel_id']])
+    return {}, 200
